97e563f66c500076dc670fac3bbbc818f4534e3f
mohsinayaz <mohxn.ayaaz@gmail.com>
2025-01-30 19:58:03 +0500

    Fix initial code css break issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgIf } from "@angular/common";
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [RouterOutlet, NgIf],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'test';
+  isLoaded = false;
+  constructor() {
+    setTimeout(() => {
+      this.isLoaded = true;
+    }, 200); // Short delay to ensure styles are loaded
+  }
 }
